2becb992cc9bcb1253927738757850087e973d73
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-03 18:38:01 +0200

    pari e dispari fine

--- Pari-e-Dispari/js/script.js
+++ Pari-e-Dispari/js/script.js
@@ -1,5 +1,42 @@
 // L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+var sceltaUtente = prompt('scegli pari o dispari');
+var sceltaNumero = parseInt(prompt('scegli ora un numero da 1 a 5'));
+console.log(sceltaUtente);
+console.log(sceltaNumero);
+
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+var randomNumber = getRandom(1, 5);
+console.log(randomNumber);
+
+function getRandom (min, max){
+  min = Math.ceil(1);
+  max = Math.floor(5);
+  var random = Math.floor(Math.random() * (max - min + min) + 1);
+  return random;
+}
+
 // Sommiamo i due numeri
+var somma = sceltaNumero + randomNumber;
+console.log(somma);
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+if(sceltaUtente == pariDispari) {
+  console.log('hai vinto');
+}
+else {
+  console.log('hai perso');
+}
+
+var pariDispari = controllo(somma);
+
+function controllo(numero) {
+  if(somma % 2 == 0){
+    var pariDispari = 'pari';
+  }
+  else {
+    var pariDispari = 'dispari';
+  }
+  return pariDispari;
+  }
+
