35b1f0d7015a35ac859439ca93fe343959fd2c7d
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-05-03 16:32:56 +0200

    palindroma

---
+++ Palindroma/js/script.js
@@ -0,0 +1,21 @@
+// Chiedere all'utente di inserire una parola
+
+var parolaUtente = prompt('inserisci una parola');
+var parolaGirata = rovesciaParola(parolaUtente);
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+if(parolaGirata == parolaUtente){
+  console.log('è palindroma');
+}else{
+  console.log('non è palindroma');
+}
+
+function rovesciaParola(parola){
+  var parolaReverse = '';
+  for(var i = parola.length -1; i>=0; i--){
+    parolaReverse += parola[i];
+  }
+  return parolaReverse;
+}
+
